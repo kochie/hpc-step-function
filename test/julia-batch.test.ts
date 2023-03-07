f3920068c3a5c42b2481a17f212c996beb3ab8dc
Robert Koch <robert@kochie.io>
2023-03-07 12:04:29 +1100

    Initial commit

---
+++ test/julia-batch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JuliaBatch from '../lib/julia-batch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/julia-batch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JuliaBatch.JuliaBatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
